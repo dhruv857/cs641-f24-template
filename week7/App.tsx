518597df64fef9d4832b602b94af03d0c2c6b8a6
dhruv857 <dgandhi@pace.edu>
2024-10-24 19:49:48 -0400

    week7 nav and tabs

--- week7/App.tsx
+++ week7/App.tsx
@@ -1,16 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {NavigationContainer} from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
-function Home() {
+function Home({navigation}) {
   return (
       <View style={styles.container}>
         <Text>This is home screen</Text>
+          <Button title={"Settings"} onPress={() => navigation.navigate('Settings')}/>
       </View>
   );
+}
+
+function Settings({navigation}) {
+    return (
+        <View style={styles.container}>
+            <Text>This is settings screen</Text>
+            <Button title={"Settings Again"} onPress={() => navigation.popTo('Settings')}/>
+        </View>
+    );
 }
 
 
@@ -20,6 +30,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Settings" component={Settings}/>
         </Stack.Navigator>
       </NavigationContainer>
   );
